6e0f37cc92e20a2784918041be2265cb944cbcff
Heru Triana <58533039+Herutriana44@users.noreply.github.com>
2024-10-14 12:16:27 +0700

    Challenge Supplier(0001)

--- fw-praktikum/routes/web.php
+++ fw-praktikum/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SupplierController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,5 +26,13 @@ Route::get('/product/{id}', [ProductController::class, 'show']);
 Route::get('/product/{id}/edit', [ProductController::class, 'edit']);
 Route::put('/product/{id}', [ProductController::class, 'update']);
 Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+
+Route::get('/supplier', [SupplierController::class, 'index']);
+Route::get('/supplier/create', [SupplierController::class, 'create'])->name("supplier-create");
+Route::post('/supplier', [SupplierController::class, 'store'])->name("supplier-store");
+Route::get('/supplier/{id}', [SupplierController::class, 'show']);
+Route::get('/supplier/{id}/edit', [SupplierController::class, 'edit']);
+Route::put('/supplier/{id}', [SupplierController::class, 'update']);
+Route::delete('/supplier/{id}', [SupplierController::class, 'destroy']);
 
 require __DIR__.'/auth.php';
